b6320fc193d77aac234fbb29675c9fb4dfd1c798
SamuelSantanaSilva <samuel.silva@pipetech.com.br>
2024-03-09 19:36:11 -0300

    estudos

--- estudos_back_end/node-js/cursos/curso-mateus/curso-exercicios-samuel/3-core-modules/2-retornando-html/index.js
+++ estudos_back_end/node-js/cursos/curso-mateus/curso-exercicios-samuel/3-core-modules/2-retornando-html/index.js
@@ -1,13 +1,13 @@
-const http = require("http")
+const http = require('http')
 
 const port = 3000
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-type', 'text/html')
-    res.end('<h1>Olá este é meu primeiro texto no servidor</h1><p>Testando Atualização</p>')
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('<h1>Olá, este é o meu primeiro server!</h1>')
 })
 
 server.listen(port, () => {
-    console.log(`Estou usando a porta ${port}`)
+  console.log(`Servidor rodando na porta: ${port}`)
 })
