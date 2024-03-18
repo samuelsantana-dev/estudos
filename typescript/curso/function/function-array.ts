0b6764e18e3d7de1b4cb21d7eb44caa1350b5ccd
SamuelSantanaSilva <samuel.silva@pipetech.com.br>
2024-03-18 09:23:50 -0300

    function array

--- typescript/curso/function/function-array.ts
+++ typescript/curso/function/function-array.ts
@@ -9,7 +9,6 @@ function somarArray(numeros: number[]): number {
 let numerosTeste: number[] = [1, 2, 3, 4, 5];
 console.log(somarArray(numeros)); // Saída: 15
 
-
 function encontrarMaior(numeros: number[]): number {
     let maior: number = numeros[0];
     for (let numero of numeros) {
@@ -23,14 +22,12 @@ function encontrarMaior(numeros: number[]): number {
 let numberss: number[] = [10, 5, 20, 8, 15];
 console.log(encontrarMaior(numeros)); // Saída: 20
 
-
 function filtrarPares(numeros: number[]): number[] {
     return numeros.filter(numero => numero % 2 === 0);
 }
 
 let number: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(filtrarPares(numeros)); // Saída: [2, 4, 6, 8, 10]
-
 
 function dobrarNumeros(numeros: number[]): number[] {
     return numeros.map(numero => numero * 2);
