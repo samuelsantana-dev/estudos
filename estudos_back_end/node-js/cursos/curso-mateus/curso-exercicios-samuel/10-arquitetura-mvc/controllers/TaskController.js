4dcc1b77369f507095f034c374f5c341487636b1
samuel-santana109 <samuelsantanadasilva8@gmail.com>
2024-04-12 18:36:09 -0300

    refactor: :recycle: update back end

--- estudos_back_end/node-js/cursos/curso-mateus/curso-exercicios-samuel/10-arquitetura-mvc/controllers/TaskController.js
+++ estudos_back_end/node-js/cursos/curso-mateus/curso-exercicios-samuel/10-arquitetura-mvc/controllers/TaskController.js
@@ -1,13 +1,26 @@
 // A minha controler nao é uma rota ela apenas é o caminho para os arquivos onde vou MOSTRAR os dados
 // Nunca deixar uma / na renderização sempre inicia pelo nome inicial
-const modelTask = require('../models/Task');
+const Task = require('../models/Task');
 
 module.exports = class TaskController {
     static createTask(req, res){
         res.render('tasks/create')
     }
 
-    static showTaks(req, res){
-        res.render('tasks/all')
+    static async saveTaskPost(req, res){
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+
+        await Task.create(task)
+        res.redirect('/tasks')
+    }
+
+    static async showTaks(req, res){
+        const task = await Task.findAll({raw: true})
+
+        res.render('tasks/all', {task})
     }
 }
--- estudos_back_end/node-js/cursos/curso-mateus/curso-exercicios-samuel/10-arquitetura-mvc/routes/taksRoutes.js
+++ estudos_back_end/node-js/cursos/curso-mateus/curso-exercicios-samuel/10-arquitetura-mvc/routes/taksRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const TaskController = require('../controllers/TaskController');
 
 router.get('/add', TaskController.createTask);
+router.post('/add', TaskController.saveTaskPost);
 router.get('/', TaskController.showTaks);
 
 module.exports = router
