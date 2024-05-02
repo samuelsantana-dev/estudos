996e0d8a69a1e156c202ec27c6275764730e42c5
samuel-santana109 <samuelsantanadasilva8@gmail.com>
2024-05-01 22:04:42 -0300

    refactor: :recycle: estudos

---
+++ estudos_back_end/node-js/cursos/curso-mateus/curso-exercicios-samuel/14-api/1-api/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Primeira rota criada com sucesso!' })
+})
+
+app.listen(3000)
